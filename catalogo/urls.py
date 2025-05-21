b2998acdf469c77c9cfaa982ed4d9b598afcac7e
sebastian cabrera <128442554+sebax401@users.noreply.github.com>
2025-05-20 23:27:31 -0400

    Delete catalogo directory

--- catalogo/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('catalogo/', views.catalogo, name='catalogo'),
-    path('Menu/', views.Menu, name='Menu'),
-    path('ListaProductos/', views.ListaProductos, name='ListaProductos'),
-    path('FiltroProductos/', views.FiltroProductos, name='FiltroProductos'),
-]
