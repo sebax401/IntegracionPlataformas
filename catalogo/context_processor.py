e7a05f08d31a8e63769acc08192e892228725968
sebastian cabrera <128442554+sebax401@users.noreply.github.com>
2025-06-24 20:56:28 -0400

    Delete catalogo directory

--- catalogo/context_processor.py
+++
@@ -1,6 +0,0 @@
-def total_carrito(request):
-    total = 0
-    if 'carrito' in request.session:
-        for value in request.session['carrito'].values():
-            total += float(value.get('precio_final', value.get('precio', 0)))
-    return {'total_carrito': total}
